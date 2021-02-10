d7342373344186671d1becce975bf9413433ca5a
Annie Kelly <anniekelly6894@gmail.com>
2021-02-10 13:56:36 -0700

    implementing live mode and recording tools

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var wav = require('node-wav');
 const WavDecoder = require("wav-decoder");
 
 var open_sockets = [];
+var files = fs.readdirSync('./recordings/');
+var numberOfFiles = files.length;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
@@ -21,26 +23,43 @@ io.on('connection', function(socket){
   console.log('a user connected');
   open_sockets.push(socket);
 
-  /* to do, eventually this needs to be a user specified event with parameters */
-  //recordAudio(socket);
-  //recordWavefile();
+  socket.emit('filenames', files);
 
-  socket.on('decode', function(){
-    decodeWavefile('./recordings/recording.wav', socket);
-});
+  socket.on('live', function() {
+    // send live stream back
+    // socket.emit live data
+  });
+
+  socket.on('trends', function() {
+    // send trends data back
+    // socket.emit trends data
+  });
+
+  socket.on('request-file', function(filename) {
+    decodeAndSendWavefile(filename, socket);
+  })
+
+  socket.on('start-recording', function() {
+    recordWavefile();
+  });
+
+  socket.on('stop-recording', function() {});
+
+  socket.on('toggle-pause-recording', function() {});
 
   socket.on('disconnect', function(){
         open_sockets = open_sockets.filter(function(item){
               return item != socket;
         });
   });
+
 });
 
 http.listen(WS_PORT, function(){
   console.log('listening for WEBSOCKET connections on *:'+WS_PORT);
 });
 
-/*function recordAudio(socket) {
+function recordAudio(socket) {
 
   var sound = new Sound({
       debug: false,
@@ -49,7 +68,6 @@ http.listen(WS_PORT, function(){
       alsa_format: 'dat',
       alsa_device: 'plughw:1,0'
   });
-
   sound.record();
 
   setTimeout(() => {
@@ -59,35 +77,41 @@ http.listen(WS_PORT, function(){
 
   }, 2000);
 
-}*/
+}
 
 function recordWavefile() {
+  var filename = './recording' + numberOfFiles.toString() + '.wav';
   var sound = new Sound({
     debug: false,
     destination_folder: './recordings/',
-    filename: './recording.wav',
+    filename: filename,
     alsa_format: 'dat',
     alsa_device: 'plughw:1,0'
   });
-
   sound.record();
 
   setTimeout(() => {
       sound.stop();
       console.log("done recording");
 
-  //    let buffer = fs.readFileSync('./recordings/recording.wav');
-  //    let result = wav.decode(buffer);
-  //    socket.emit("recording", buffer);
+      var files = fs.readdirSync('./recordings/');
+      var numberOfFiles = files.length;
 
-      decodeWavefile('./recordings/recording.wav', socket);
+      socket.emit("new-recorded-file", filename);
+
+      //    let buffer = fs.readFileSync('./recordings/recording.wav');
+      //    let result = wav.decode(buffer);
+      //    socket.emit("recording", buffer);
+
+      //decodeAndSendWavefile(filename, socket);
 
       //socket.emit("recording", "hi");
 
   }, 2000);
 }
 
-function decodeWavefile(filepath, socket) {
+function decodeAndSendWavefile(filename, socket) {
+  var filepath = './recordings/' + filename.toString();
   const readFile = (filepath) => {
     return new Promise((resolve, reject) => {
       fs.readFile(filepath, (err, buffer) => {
@@ -105,7 +129,8 @@ function decodeWavefile(filepath, socket) {
     console.log(audioData.sampleRate);
     console.log(audioData.channelData[0]); // Float32Array
     //console.log(audioData.channelData[1]); // Float32Array
-    socket.emit("recording", audioData);
+    var msg = {filename: filename, audioData: audioData};
+    socket.emit("receivefile", msg);
   });
 }
 
--- public/petsounds.js
+++ public/petsounds.js
@@ -4,6 +4,86 @@ var socket = io();
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)({sampleRate: 250000});
 var analyser = audioCtx.createAnalyser();
 
+socket.on('livedata', function(stream) {
+  // update view with live data
+});
+
+socket.on('filenames', function(files) {
+  var namesListElement = document.getElementById("filenames");
+  for (var i = 0; i < files.length; i++) {
+    var f = files[i];
+    namesListElement.innerHTML = namesListElement.innerHTML + '<li class="nav-item"><a class="nav-link js-scroll-trigger" onclick="fileSelected(\'' + f.toString() + '\')" href="#"><span class="nav-item-primary-icon"><i class="fas fa-headphones-alt"></i></span><span class="nav-item-primary-label">'+ f.toString() +'</span></a></li>'
+  }
+})
+
+socket.on('new-recorded-file', function(filename) {
+  namesListElement.innerHTML = namesListElement.innerHTML + '<li class="nav-item"><a class="nav-link js-scroll-trigger" onclick="fileSelected(\'' + filename.toString() + '\')" href="#"><span class="nav-item-primary-icon"><i class="fas fa-headphones-alt"></i></span><span class="nav-item-primary-label">'+ filename.toString() +'</span></a></li>'
+});
+
+socket.on('receivefile', function(msg) {
+  var filename = msg["filename"];
+  var audioData = msg["audioData"];
+  audioCtx.resume();
+
+  /* Clear current graphs */
+  document.getElementById("recording-title").innerHTML = filename.toString();
+  document.getElementById("waveform").innerHTML = "";
+  document.getElementById("spectrum").innerHTML = "";
+  document.getElementById("wave-spectrogram").innerHTML = "";
+
+  /* Convert channel data to Float32Array */
+  var channelArray = Object.values(audioData.channelData[0]);
+  var channelFloatArray = new Float32Array(channelArray);
+
+  /* Create audio buffer and fill it with decoded wav file data (type Float32 array) */
+  var buffer = audioCtx.createBuffer(numChannels, channelFloatArray.length, audioData.sampleRate);
+  buffer.copyToChannel(channelFloatArray, 0, 0);
+  //console.log(buffer.getChannelData(0));
+
+  /* start of custom waveform code (not working), feel like things not connecting to analyser properly */
+  /* Create audio source node and connect to analyser */
+  /*var source = new AudioBufferSourceNode(
+    audioCtx, {
+      buffer: buffer
+    });*/
+  var source = audioCtx.createBufferSource();
+  source.buffer = buffer;
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  //source.start(); // plays the recording back
+  //analyser.start();
+
+  analyser.fftSize = 2048;
+  analyser.smoothingTimeConstant = 0;
+  var bufferLength = analyser.frequencyBinCount; // half of fftSize
+  var dataArray = new Float32Array(bufferLength);
+  analyser.getFloatTimeDomainData(dataArray);
+
+  generateWaveform(bufferLength, dataArray);
+  /* end of custom waveform code (not working) */
+  
+  var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'black',
+    progressColor: '#31D0AA',
+    audioContext: audioCtx,
+    barWidth: 1,
+    barRadius: 2,
+    plugins: [
+        WaveSurfer.spectrogram.create({
+            wavesurfer: wavesurfer,
+            container: "#wave-spectrogram",
+            labels: true
+        })
+    ]
+});
+
+  wavesurfer.loadDecodedBuffer(buffer);
+  //wavesurfer.loadArrayBuffer(channelArray);
+  //wavesurfer.load('http://wavesurfer-js.org/example/media/demo.wav');
+  wavesurfer.drawBuffer();
+});
+
 socket.on('recording', function(audioData){
   console.log("received socket msg");
   audioCtx.resume();
@@ -60,3 +140,10 @@ socket.on('recording', function(audioData){
   //wavesurfer.load('http://wavesurfer-js.org/example/media/demo.wav');
   wavesurfer.drawBuffer();
 });
+
+function fileSelected(filename) {
+  socket.emit('request-file', filename);
+  $("#recorded-file-view").show();
+  $("#trends-view").hide();
+  $("#live-view").hide();
+}
